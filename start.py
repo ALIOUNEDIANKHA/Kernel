300fd556a08293f9bdfbbf8eb4da705723454e28
BEN <99078267+ALIOUNEDIANKHA@users.noreply.github.com>
2022-07-06 10:23:12 +0000

    Delete start.py

--- start.py
+++
@@ -1,9 +0,0 @@
-import sys
-import main
-
-if __name__=="__main__":
-    print('Kernel Logistic Ridge Regression(KLRR) or Kernel Ridge Regression(KRR) ?')
-    a = sys.argv
-    print('You chose ',a)
-    main.a()
-
